d81d9e517e3ebe6cb2f0d27fb33b95f75187679d
Linh <linh.nv@samsung.com>
2015-10-05 16:55:10 +0700

    native

---
+++ problem16.py
@@ -0,0 +1,10 @@
+sum = 0
+for n in str(2**1000):
+	sum += int(n)
+print sum
+
+L = ["Ha Noi", "Hai Phong", "TP Ho Chi Minh"]
+for thanhPho in L:
+	print thanhPho
+	
+#print sum(map(int, str(2 ** 1000)))
---
+++ test.py
@@ -0,0 +1,14 @@
+print 2**3
+print 2==3
+print 2 <>3
+print 2 != 3
+print oct(13)
+a = 00111100
+
+b = 00001101
+c = 00001101
+
+print ~a 
+print a &b
+print bin(c)[2:]
+print c
