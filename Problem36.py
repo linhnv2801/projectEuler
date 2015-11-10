bd168de3f416d59bbaa753f2a9f6d7386017aab1
Linh <linh.nv@samsung.com>
2015-11-10 11:21:45 +0700

    still error problem 36

---
+++ Problem36.py
@@ -0,0 +1,21 @@
+def checkPalindromic(num):
+    s = str(num)
+    len2 = len(s)
+    mid = len2 / 2
+    for i in range(0, mid):
+        if s[i] != s[len2 - i - 1]:
+            return "false"
+    return "true"
+
+
+def toBin(num):
+    # return '{0:08b}'.format(num)
+    return bin(num)[2:]
+
+
+count = 0
+for i in range(1000000):
+    if checkPalindromic(i) == "true" and checkPalindromic(toBin(i)) == "true":
+        count += 1
+
+print count
